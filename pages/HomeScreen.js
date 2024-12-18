e114a93201e6d47b179b242b9a6bd5288e11b7e4
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2024-12-18 12:49:35 +0700

    Remove files

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -2,17 +2,9 @@ import React from "react";
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
-  const handlePress = async() => {
-    try {
-      const response = await fetch('http://192.168.10.159:3000/');
-      const data = await response.json();
-      Alert.alert('Data from Backend', data.name);
-    } catch(error) {
-      Alert.alert('API Unsuccessfully!');
-    }
-  };
     return (
       <View style={styles.container}>
+
         <Text>Welcome to the Home Screen!</Text>
         <Button
           title="Go to Second Screen"
@@ -29,4 +21,5 @@ export default function HomeScreen({ navigation }) {
       alignItems: 'center',
       justifyContent: 'center',
     },
+
   });
